f12c2920399ebe99a2428e511c08f2373ad8364c
guguwhite <guguwhite@icloud.com>
2020-07-13 13:51:08 +0800

    2

--- SpringBoot_Hotel/src/main/java/com/hotel/controller/TypeController.java
+++ SpringBoot_Hotel/src/main/java/com/hotel/controller/TypeController.java
@@ -1,5 +1,55 @@
 package com.hotel.controller;
 
-public class TypeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.hotel.entity.Type;
+import com.hotel.service.TypeService;
+
+@Controller
+@RequestMapping("**.do")
+public class TypeController {
+	TypeService typeService;
+
+	public TypeService getTypeService() {
+		return typeService;
+	}
+
+	@Autowired
+	public void setTypeService(TypeService typeService) {
+		this.typeService = typeService;
+	}
+	
+	@RequestMapping("showtype")
+	public String  showType(Model model)
+	{
+		List<Type> list = typeService.findAll();
+		model.addAttribute("list", list);
+		return "type";
+	}
+	
+	@RequestMapping("addtype")
+	public String addType(Type type)
+	{
+		typeService.add(type);
+		return "forward:showtype.do";
+	}
+
+	@RequestMapping("modifytype")
+	public String modifyType(Type type)
+	{
+		typeService.update(type);
+		return "forward:showtype.do";
+	}
+	
+	@RequestMapping("removetype")
+	public String removeType(int typeId)
+	{
+		typeService.delete(typeId);
+		return "forward:showtype.do";
+	}
+	
 }
